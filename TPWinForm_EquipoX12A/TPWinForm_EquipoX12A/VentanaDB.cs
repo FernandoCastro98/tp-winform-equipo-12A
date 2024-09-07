cc331c23205b88588399c6e2d33d97ef2d1ede7c
FernandoCastro98 <sebastianfernandocastro@outlook.es>
2024-09-06 18:35:59 -0600

    Port proyecto con el nombre mal

---
+++ TPWinForm_EquipoX12A/TPWinForm_EquipoX12A/VentanaDB.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+
+namespace TPWinForm_EquipoX12A
+{
+    
+    public partial class VentanaDB : Form
+    {
+        private List<Articulo> Articulos;
+        public VentanaDB()
+        {
+            InitializeComponent();
+        }
+
+        private void VentanaDB_Load(object sender, EventArgs e)
+        {
+            pnlFondo.Visible = false;
+        }
+
+        private void btnMostrarTodo_Click(object sender, EventArgs e)
+        {
+            ArticuloNegocio ArtNeg = new ArticuloNegocio();
+
+            Articulos = ArtNeg.listar();
+            pbxProducto.Load(Articulos[0].UrlImagen);
+            dgvArticulos.DataSource = Articulos;
+            dgvArticulos.Columns["IdCategoria"].Visible = false;
+            dgvArticulos.Columns["IdMarca"].Visible = false;
+            dgvArticulos.Columns["UrlImagen"].Visible = false;
+        }
+
+        private void btnAgregarArticulo_Click(object sender, EventArgs e)
+        {
+            pnlFondo.Visible = true;
+            
+        }
+
+        private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+           
+            
+            try
+            {
+                Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
+                pbxProducto.Load(seleccionado.UrlImagen);
+            }
+            catch (Exception)
+            {
+                pbxProducto.Load("https://plus.unsplash.com/premium_photo-1682310093719-443b6fe140e8?q=80&w=1824&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+            }
+        }
+
+        private void btnUrlCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string urlImagen = txbUrlImagen.Text;
+                pbxUrlImagen.Load(urlImagen);
+            }
+            catch (Exception)
+            {
+
+                pbxUrlImagen.Load("https://plus.unsplash.com/premium_photo-1682310093719-443b6fe140e8?q=80&w=1824&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+
+            }
+
+
+
+        }
+    }
+}
---
+++ TPWinForm_EquipoX12A/dominio/Articulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+
+{
+    public class Articulo
+    {
+        public string Codigo {  get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        public int IdMarca { get; set; }
+        public Marca Marca { get; set; }
+        public int IdCategoria { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        
+    }
+}
